bddd573cb420ccf6811d411db3b01c336a21590d
momobiz <mohamedaliannabi35@gmail.com>
2020-10-09 14:00:58 +0200

    v0

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import './App.css';
+import Chrono from "./composants/Chrono";
+
+class App extends React.Component {
+
+
+  render(){
+    return (
+      <div className="App">
+       <h2>salut </h2>
+       <Chrono/>
+
+       
+      </div>
+    )
+
+  }
+
+ 
+}
+
+
+export default App;
